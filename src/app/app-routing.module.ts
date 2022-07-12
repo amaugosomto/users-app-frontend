99b5a2e06ad419b70a6f06830be62c39144ca50a
amaugosomto <amaugosomto@gmail.com>
2022-07-12 22:11:16 +0200

    added auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) }
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: '',   redirectTo: 'users', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' },
 ];
 
 @NgModule({
